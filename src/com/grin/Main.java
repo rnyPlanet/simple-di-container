ad337449a47e90959274127b953de920a7fa54ba
Stanislav Kotyhrobov <workautu@gmail.com>
2021-11-19 20:28:14 +0200

    Removed unnecessary imports

--- src/com/grin/Main.java
+++ src/com/grin/Main.java
@@ -2,11 +2,6 @@ package com.grin;
 
 import com.grin.services.ServiceA;
 import com.grin.services.ServiceB;
-import com.grin.services.impl.ServiceAImpl;
-import com.grin.services.impl.ServiceBImpl;
-
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
 
