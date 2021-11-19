2217c0b195c8faabdfe4de8d3063fdddcaa81250
Stanislav Kotyhrobov <workautu@gmail.com>
2021-11-19 19:44:22 +0200

    Create Context

--- src/com/grin/Main.java
+++ src/com/grin/Main.java
@@ -1,55 +1,36 @@
 package com.grin;
 
 import com.grin.services.ServiceA;
+import com.grin.services.ServiceB;
 import com.grin.services.impl.ServiceAImpl;
 import com.grin.services.impl.ServiceBImpl;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
 
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws Exception {
+        DIContext context = createContext();
+        perform(context);
+    }
+
+
+    private static DIContext createContext() throws Exception {
         Set<Class<?>> serviceClasses = new HashSet<>();
+
         serviceClasses.add(ServiceAImpl.class);
         serviceClasses.add(ServiceBImpl.class);
 
-        ServiceA serviceA = createServiceA(serviceClasses);
-
-        System.out.println(serviceA.perform());
+        return new DIContext(serviceClasses);
     }
 
-    private static ServiceA createServiceA(Set<Class<?>> serviceClasses) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        Set<Object> serviceInstances = new HashSet<>();
-        for (Class<?> serviceClass : serviceClasses) {
-            Constructor<?> constructor = serviceClass.getConstructor();
-            constructor.setAccessible(true);
-            serviceInstances.add(constructor.newInstance());
-        }
+    private static void perform(DIContext context) {
+        ServiceA serviceA = context.getServiceInstance(ServiceA.class);
+        ServiceB serviceB = context.getServiceInstance(ServiceB.class);
 
-        for (Object serviceInstance : serviceInstances) {
-            for (Field field : serviceInstance.getClass().getDeclaredFields()) {
-                Class<?> fieldType = field.getType();
-                field.setAccessible(true);
-
-                for (Object matchPartner : serviceInstances) {
-                    if (fieldType.isInstance(matchPartner)) {
-                        field.set(serviceInstance, matchPartner);
-                    }
-                }
-            }
-        }
-
-        for (Object serviceInstance : serviceInstances) {
-            if (serviceInstance instanceof ServiceA) {
-                return (ServiceA) serviceInstance;
-            }
-        }
-
-        return null;
+        System.out.println(serviceA.perform());
+        System.out.println(serviceB.perform());
     }
 
 }
